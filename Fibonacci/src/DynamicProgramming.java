a04f1ff7d1284a00029ce38075045465f8e6ac80
sxxmin <sxxmin@sxxminui-MacBookPro.local>
2019-09-24 14:26:56 +0900

    Fill in the DynamicProgramming class

--- Fibonacci/src/DynamicProgramming.java
+++ Fibonacci/src/DynamicProgramming.java
@@ -1,2 +1,22 @@
+import java.math.BigInteger;
+
 public class DynamicProgramming {
+    public final static int MAXSIZE = 100_000;
+    public BigInteger[] array = new BigInteger[MAXSIZE];
+
+    public DynamicProgramming() {
+        this.array[0] = BigInteger.ZERO;
+        this.array[1] = BigInteger.ONE;
+    }
+
+    public BigInteger fibonacci(int n) {
+
+        if (n == 0) return array[0];
+        if (n == 1) return array[1];
+
+        for (int i = 2; i <= n; ++i) {
+            array[i] = array[i - 1].add(array[i - 2]);
+        }
+        return array[n];
+    }
 }
