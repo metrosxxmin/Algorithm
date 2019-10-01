ad8bfae2d23e5d755f84890e9d840649f10e8a7e
metrosxxmin <sxxmin.lee@gmail.com>
2019-10-01 11:51:41 +0900

    Fill in the main for testing all method including FomulaForConstant

--- Fibonacci/src/Main.java
+++ Fibonacci/src/Main.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     static BigInteger result;
+    static long resultForconstant;
     static long startTime, endTime;
-    final static int INDEXOFMAX = 1000;
+    final static int INDEXOFMAX = 70;
 
     public static void main(String[] argc) {
         System.out.printf("Method \n1 : Recursion \n2 : Dynamic Programming " +
                 "\n3 : Space optimize \n4 : Power of Matrix \n5 : Optimaized Power of Matrix " +
-                "\n6 : Using formula for Log N\n\ninput : ");
+                "\n6 : Using formula for Log N \n7 : Using formula for constant\n\ninput : ");
         Scanner scanner = new Scanner(System.in);
         int input = scanner.nextInt();
 
@@ -87,11 +88,28 @@ public class Main {
             }
         }
 
+        if (input == 7) {
+            FomulaForConstant fomulaForConstant = new FomulaForConstant();
+            for (int i = 0; i <= INDEXOFMAX; ++i) {
+
+                startTime = System.nanoTime();
+                resultForconstant = fomulaForConstant.fibonacci(i);
+                endTime = System.nanoTime();
+
+                printLine(i, resultForconstant, endTime - startTime);
+            }
+        }
     }
 
     // print format
     public static void printLine(int n ,BigInteger bigInteger ,long time) {
         System.out.printf("f<%2d> = %-20d", n, bigInteger);
+        System.out.printf(String.format("%20.12f", time / 1_000_000_000.0) + " sec \n");
+    }
+
+    // print format for fomulaforconstatnt
+    public static void printLine(int n ,long value ,long time) {
+        System.out.printf("f<%2d> = %-20d", n,value);
         System.out.printf(String.format("%20.12f", time / 1_000_000_000.0) + " sec \n");
     }
 }
