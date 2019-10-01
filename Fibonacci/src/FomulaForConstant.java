9a4f403d441075495bc4a1f4b8a9409bc8c3528d
metrosxxmin <sxxmin.lee@gmail.com>
2019-10-01 11:43:29 +0900

    Add code in FomulaForConstant class

--- Fibonacci/src/FomulaForConstant.java
+++ Fibonacci/src/FomulaForConstant.java
@@ -1,7 +1,14 @@
-import java.math.BigInteger;
 
 // Fn = {[(√5 + 1)/2] ^ n} / √5
 
 public class FomulaForConstant {
 
+    public final static double phi = (1 + Math.sqrt(5)) / 2;
+
+    public long fibonacci(int n) {
+        return (long)(Math.round(Math.pow(phi, n)) / Math.sqrt(5));
+    }
+
 }
+
+
