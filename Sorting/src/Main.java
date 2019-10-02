3d89b6c12917bec8f9197926c9ee16840dc36d68
metrosxxmin <sxxmin.lee@gmail.com>
2019-10-02 20:37:21 +0900

    Add test sample in main method

--- Sorting/src/Main.java
+++ Sorting/src/Main.java
@@ -1,6 +1,17 @@
 
 public class Main {
+
+    public final static int MAX_SIZE = 1_000;
+    public static int[] testArray = new int[MAX_SIZE];
+
     public static void main(String[] args) {
-        
+
+        for (int i = 0; i < MAX_SIZE; ++i) {
+            testArray[i] = (int)((Math.random() * 1_000) + 1);
+        }
+
+        for (int i = 0; i < MAX_SIZE; ++i) {
+            System.out.print(testArray[i] + " ");
+        }
     }
 }
