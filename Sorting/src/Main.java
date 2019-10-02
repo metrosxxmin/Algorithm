8e383c49e55ea055a7f69c19f0270952284abe07
metrosxxmin <sxxmin.lee@gmail.com>
2019-10-03 07:41:11 +0900

    Add a testing frame in main.

--- Sorting/src/Main.java
+++ Sorting/src/Main.java
@@ -1,17 +1,38 @@
+import java.util.Scanner;
 
 public class Main {
 
-    public final static int MAX_SIZE = 1_000;
+    public static int[] result;
+    public static long startTime, endTime;
+    public final static int MAX_SIZE = 50_000;
     public static int[] testArray = new int[MAX_SIZE];
 
     public static void main(String[] args) {
 
         for (int i = 0; i < MAX_SIZE; ++i) {
-            testArray[i] = (int)((Math.random() * 1_000) + 1);
+            testArray[i] = (int)((Math.random() * 50_000) + 1);
         }
 
+        System.out.print("Before sorting \n>> ");
         for (int i = 0; i < MAX_SIZE; ++i) {
             System.out.print(testArray[i] + " ");
+        }
+
+        System.out.print("\n\n <Option> \n1 : Insertion \n\ninput : ");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+
+        Sort sort = new Sort(testArray);
+        if (input == 1) {
+            startTime = System.nanoTime();
+            result = sort.byInsertion(testArray);
+            endTime = System.nanoTime();
+
+            System.out.print("After sorting \n>> ");
+            for (int i = 0; i < MAX_SIZE; ++i) {
+                System.out.print(testArray[i] + " ");
+            }
+            System.out.print("\nRUNTIME : " + String.format("%20.12f", (endTime - startTime) / 1_000_000_000.0) + " sec \n");
         }
     }
 }
