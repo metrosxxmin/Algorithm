0151c31ef91fa896a73858619778b605d0c85ac5
metrosxxmin <sxxmin.lee@gmail.com>
2019-10-04 02:02:55 +0900

    Add a method for bubble sort in Sort class.

--- Sorting/src/Sort.java
+++ Sorting/src/Sort.java
@@ -23,4 +23,20 @@ public class Sort {
 
         return arr;
     }
+
+    public int[] byBubble(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; ++i) {
+            for (int j = 0; j < n - i - 1; ++j) {
+                if (arr[j] > arr[i]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        return arr;
+    }
 }
